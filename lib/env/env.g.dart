99cac4025edb559e5fb2a9d4e6cff1d6b1c5742a
iamudesharma <udesh2568@gmail.com>
2023-04-08 20:44:38 +0530

    first commit

---
+++ lib/env/env.g.dart
@@ -0,0 +1,11 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+class _Env {
+  static const apiKey = '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';
+}
